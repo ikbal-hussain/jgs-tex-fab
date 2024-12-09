ff7cd2ce85c1fb19ba4b571583ebe67f5d597111
Ikbal Hussain <166986889+ikbal-hussain@users.noreply.github.com>
2024-12-09 09:48:56 +0530

    Update LoadingIndicator.jsx

--- frontend/src/components/LoadingIndicator.jsx
+++ frontend/src/components/LoadingIndicator.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoadingIndicator = () => {
-  return <div className="loading">Loading...</div>;
+  return <div className="loading">Loading...(Fetching data takes about 1 minute when first started after long time)</div>;
 };
 
 export default LoadingIndicator;
